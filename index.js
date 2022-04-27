dd444d2794890aa8393fb0f8c4f9f6a7f6d5ac56
Rahul Yadav <rahulkyadav9691@gmail.com>
2022-04-28 01:15:40 +0530

    add mongodb

--- index.js
+++ index.js
@@ -12,48 +12,47 @@ app.use(cors());
 
 mongoose.connect(
     "mongodb+srv://itzrahulyadav:rahul123@mern.8pjc4.mongodb.net/tutorialmern?retryWrites=true&w=majority",
-{useNewUrlParser:true}
-).then(()=>{
+    { useNewUrlParser: true }
+).then(() => {
     console.log("connected to database");
 });
 
 
-app.post('/addFriend',async(req,res)=>{
+app.post('/addFriend', async (req, res) => {
     const name = req.body.name;
     const age = req.body.age;
 
-    const friend = new FriendModel({name:name,age:age});
+    const friend = new FriendModel({ name: name, age: age });
     await friend.save();
     res.send('success');
 });
 
-app.get('/read',async (req,res)=>{
-   FriendModel.find({},(err,result)=>{
-       if(err)res.send(err);
-       else res.send(result);
-   })
+app.get('/read', async (req, res) => {
+    FriendModel.find({}, (err, result) => {
+        if (err) res.send(err);
+        else res.send(result);
+    })
 });
 
-app.put('/update',(req,res) => {
+app.put('/update', (req, res) => {
     const newAge = req.body.newAge;
     const id = req.body.id;
-    try{
-      FriendModel.findById(id,(err,friendToUpdate)=>{
-          friendToUpdate.age = Number(newAge);
-          friendToUpdate.save();
-      })
-    }catch(err)
-    {
+    try {
+        FriendModel.findById(id, (err, friendToUpdate) => {
+            friendToUpdate.age = Number(newAge);
+            friendToUpdate.save();
+        })
+    } catch (err) {
         console.log(err);
     }
     res.send("updated");
 });
-app.delete('/delete/:id',async(req,res)=>{
+app.delete('/delete/:id', async (req, res) => {
     const id = req.params.id;
     await FriendModel.findByIdAndRemove(id).exec();
     res.send("deleted successfully");
 })
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, () => {
     console.log("listning to the port at 3001");
 })
